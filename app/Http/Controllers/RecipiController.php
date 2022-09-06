ec0be18aff60f8eea70da6ec4d0976760f9f5ba8
taniguchiryuzo <kousei0038@gmail.com>
2022-09-07 01:04:55 +0900

    管理者側のページ実装

---
+++ app/Http/Controllers/RecipiController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Recipi;
+use Illuminate\Http\Request;
+
+class RecipiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $recipis = Recipi::all();
+        return response()->json([
+            'message' => 'ok',
+            'data'    => $recipis,
+        ], 200, [], JSON_UNESCAPED_UNICODE);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use  App\Http\Controllers\Admin\AdminRecipiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//レシピ
+Route::get('/admin/recipis', [AdminRecipiController::class, 'index'])->name('admin.recipis.index');
+Route::get('/admin/recipis/create', [AdminRecipiController::class, 'create'])->name('admin.recipis.create');
+Route::post('/admin/recipis', [AdminRecipiController::class, 'store'])->name('admin.recipis.store');
+Route::get('/admin/recipis/{recipi}', [AdminRecipiController::class, 'edit'])->name('admin.recipis.edit');
